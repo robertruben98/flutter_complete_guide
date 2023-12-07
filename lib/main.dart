b312765d5a5426275be1754d7c762364f11260ab
Robert <robertruben98@gmail.com>
2023-12-07 10:39:23 -0500

    ADD: final vs const

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,44 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State {
-  String title = "APK para jenny";
-  var _questionIndex = 0;
-  var questions = [
-    {
-      'title': "What's your favorite color?",
-      'answers': [
-        "Blue",
-        "Black",
-        "White",
-      ]
-    },
-    {
-      'title': "What's your favorite animal?",
-      'answers': ["Lion", "Dolphin", "Tiger"]
-    },
-  ];
+// final is a variable that doesn't change. If doesn't change after created
+// const compile time constant, not like final, that is runtime constant. Already after you wrote the code is locked
+// all is a object
+// const question = const []. Addresses to object in memory.
+// var questions = const []. You can assign a new value for questions. But const in value can be change
 
-  void _answerQuestion() {
-    setState(() {
-      if (_questionIndex < questions.length - 1) {
-        _questionIndex = _questionIndex + 1;
-      } else {
-        _questionIndex = 0;
-      }
-    });
-  }
+class _MyAppState extends State {
+  final String title = "APK para jenny";
+  var _questionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    // const build time is a runtime constant
+    const questions = [
+      {
+        'title': "What's your favorite color?",
+        'answers': [
+          "Blue",
+          "Black",
+          "White",
+        ]
+      },
+      {
+        'title': "What's your favorite animal?",
+        'answers': ["Lion", "Dolphin", "Tiger"]
+      },
+    ];
+
+    void _answerQuestion() {
+      setState(() {
+        if (_questionIndex < questions.length - 1) {
+          _questionIndex = _questionIndex + 1;
+        } else {
+          _questionIndex = 0;
+        }
+      });
+    }
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
