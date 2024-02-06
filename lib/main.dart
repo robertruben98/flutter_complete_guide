0da547f53c7dfeeac036dac0ddf1aa0316bef259
Robert <robertruben98@gmail.com>
2024-02-06 07:54:52 -0500

    ADD: combining widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
-import 'apps/quiz/quiz_app.dart';
+// import 'apps/quiz/quiz_app.dart';
 
-void main() => runApp(QuizApp());
+// void main() => runApp(QuizApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Flutter App')),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Card(child: Text('LIST OF TX'))
+          ],
+        ));
+  }
+}
