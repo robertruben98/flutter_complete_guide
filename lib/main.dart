8eb957d4a7174f82ea9bdf096fda145d64e47261
Robert <robertruben98@gmail.com>
2024-02-11 13:39:41 -0500

    ADD: styling a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transactions.dart';
 
 // import 'apps/quiz/quiz_app.dart';
 
@@ -13,6 +14,18 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't1',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now()),
+    Transaction(
+        id: 't1', title: 'Old Shoes', amount: 69.99, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +42,27 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-                color: Colors.red,
-                child: Text('LIST OF TX'))
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
+                        child: Text(tx.amount.toString())),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString())
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            )
           ],
         ));
   }
