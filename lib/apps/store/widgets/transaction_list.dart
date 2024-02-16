359510e64ffc98421d1af4fa983b3a7b6dfcbffe
Robert <robertruben98@gmail.com>
2024-02-16 12:08:14 -0500

    ADD: show loading image

--- lib/apps/store/widgets/transaction_list.dart
+++ lib/apps/store/widgets/transaction_list.dart
@@ -12,42 +12,56 @@ class TransactionListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          var tx = transactions[index];
-          return Card(
-              child: Row(
-                children: [
-                  Container(
-                      margin:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
-                      child: Text(
-                        '\$ ' + tx.amount.toStringAsFixed(2),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(tx.title,
-                          style: Theme.of(context).textTheme.headline6
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  )
-                ],
-              ));
-        },
-        itemCount: transactions.length
-      ),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No Transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 10), // Empty component for add size
+                Container(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                var tx = transactions[index];
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).primaryColor,
+                                width: 2)),
+                        child: Text(
+                          '\$ ' + tx.amount.toStringAsFixed(2),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor),
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title,
+                            style: Theme.of(context).textTheme.headline6),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ));
+              },
+              itemCount: transactions.length),
     );
   }
 }
